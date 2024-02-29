a2a917300af161de1570fd3f9ac0d6d1ffb274e8
ramalloc <roshangupta1887@gmail.com>
2024-02-29 13:42:58 +0530

    Debugged Dashboard

--- src/pages/admin/dashboard/DashboardTab.jsx
+++ src/pages/admin/dashboard/DashboardTab.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import myContext from '../../../context/data/myContext';
 import Layout from '../../../components/layout/Layout';
@@ -11,8 +11,6 @@ function DashboardTab() {
     const context = useContext(myContext)
     const { mode, product, edithandle, deleteProduct, order, user } = context
 
-
-    // console.log(product)
     let [isOpen, setIsOpen] = useState(false)
 
     function closeModal() {
@@ -32,11 +30,13 @@ function DashboardTab() {
             <div className="container mx-auto">
                 <div className="tab container mx-auto ">
                     <Tabs defaultIndex={0} className=" " >
-                        <TabList className="md:flex md:space-x-8 bg-  grid grid-cols-2 text-center gap-4   md:justify-center mb-10 ">
+                        <TabList className="md:flex md:space-x-8 bg- grid grid-cols-2 text-center gap-4 md:justify-center mb-10 ">
                             <Tab>
                                 <button type="button" className="font-medium border-b-2 hover:shadow-purple-700 border-purple-500 text-purple-500 rounded-lg text-xl shadow-[inset_0_0_8px_rgba(0,0,0,0.6)]  px-5 py-1.5 text-center bg-[#605d5d12] ">
                                     <div className="flex gap-2 items-center">
-                                        <MdOutlineProductionQuantityLimits />Products</div> </button>
+                                        <MdOutlineProductionQuantityLimits />Products
+                                    </div>
+                                </button>
                             </Tab>
                             <Tab>
                                 <button type="button" className="font-medium border-b-2 border-pink-500 bg-[#605d5d12] text-pink-500  hover:shadow-pink-700  rounded-lg text-xl shadow-[inset_0_0_8px_rgba(0,0,0,0.6)]    px-5 py-1.5 text-center ">
@@ -95,7 +95,7 @@ function DashboardTab() {
                                         {product.map((item, index) => {
                                             const { title, price, imageUrl, category, description, date } = item;
                                             return (
-                                                <tbody className=''>
+                                                <tbody key={index} className=''>
                                                     <tr className="bg-gray-50 border-b  dark:border-gray-700" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                                                         <td className="px-6 py-4 text-black " style={{ color: mode === 'dark' ? 'white' : '' }}>
                                                             {index + 1}
@@ -123,7 +123,6 @@ function DashboardTab() {
                                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                                         </svg>
                                                                     </div>
-
                                                                     <Link to={'/updateproduct'}>
                                                                         <div onClick={() => edithandle(item)}  >
                                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -131,25 +130,21 @@ function DashboardTab() {
                                                                             </svg>
                                                                         </div>
                                                                     </Link>
-
                                                                 </div>
                                                             </div>
                                                         </td>
                                                     </tr>
-
                                                 </tbody>
                                             )
                                         })}
                                     </table>
-
                                 </div>
                             </div>
                         </TabPanel>
-                                        {/* Order */}
+                        {/* Order */}
                         <TabPanel>
                             <div className="relative overflow-x-auto mb-16">
                                 <h1 className='text-center mb-5 text-3xl font-semibold underline' style={{ color: mode === 'dark' ? 'white' : '' }}>Order Details</h1>
-
                                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                     <thead className="text-xs text-black uppercase bg-gray-200" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
                                         <tr>
@@ -158,10 +153,9 @@ function DashboardTab() {
                                             <th scope="col" className="px-6 py-3">Title</th>
                                             <th scope="col" className="px-6 py-3">Price</th>
                                             <th scope="col" className="px-6 py-3">Category</th>
-                                            <th scope="col" className="px-6 py-3">Name</th>
+                                            <th scope="col" className="px-6 py-3">Customer</th>
                                             <th scope="col" className="px-6 py-3">Address</th>
-                                            <th scope="col" className="px-6 py-3">Pincode</th>
-                                            <th scope="col" className="px-6 py-3">Phone Number</th>
+                                            <th scope="col" className="px-6 py-3">Contact</th>
                                             <th scope="col" className="px-6 py-3">Email</th>
                                             <th scope="col" className="px-6 py-3">Date</th>
                                         </tr>
@@ -170,6 +164,7 @@ function DashboardTab() {
                                         {order.map((allorder, index) => {
                                             return allorder.cartItems.map((item, itemIndex) => {
                                                 const { title, description, category, imageUrl, price } = item;
+                                                const { name, address, pincode, phoneNumber, email, date } = allorder.addressInfo;
                                                 return (
                                                     <tr key={`${index}-${itemIndex}`} className="bg-gray-50 border-b dark:border-gray-700" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '' }}>
                                                         <td className="px-6 py-4 text-black">{index * allorder.cartItems.length + itemIndex + 1}</td>
@@ -177,12 +172,11 @@ function DashboardTab() {
                                                         <td className="px-6 py-4 text-black">{title}</td>
                                                         <td className="px-6 py-4 text-black">₹{price}</td>
                                                         <td className="px-6 py-4 text-black">{category}</td>
-                                                        <td className="px-6 py-4 text-black">{allorder.addressInfo.name}</td>
-                                                        <td className="px-6 py-4 text-black">{allorder.addressInfo.address}</td>
-                                                        <td className="px-6 py-4 text-black">{allorder.addressInfo.pincode}</td>
-                                                        <td className="px-6 py-4 text-black">{allorder.addressInfo.phoneNumber}</td>
-                                                        <td className="px-6 py-4 text-black">{allorder.email}</td>
-                                                        <td className="px-6 py-4 text-black">{allorder.date}</td>
+                                                        <td className="px-6 py-4 text-black">{name}</td>
+                                                        <td className="px-6 py-4 text-black">{address}</td>
+                                                        <td className="px-6 py-4 text-black">{phoneNumber}</td>
+                                                        <td className="px-6 py-4 text-black">{email}</td>
+                                                        <td className="px-6 py-4 text-black">{date}</td>
                                                     </tr>
                                                 )
                                             })
@@ -193,7 +187,6 @@ function DashboardTab() {
                         </TabPanel>
 
                         <TabPanel>
-                            {/* <User addressInfo={addressInfo} setAddressInfo={setAddressInfo} setLoading={setLoading} /> */}
                             <div className="relative overflow-x-auto mb-10">
                                 <h1 className=' text-center mb-5 text-3xl font-semibold underline' style={{ color: mode === 'dark' ? 'white' : '' }}>User Details</h1>
                                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -202,7 +195,6 @@ function DashboardTab() {
                                             <th scope="col" className="px-6 py-3">
                                                 S.No
                                             </th>
-
                                             <th scope="col" className="px-6 py-3">
                                                 Name
                                             </th>
@@ -212,13 +204,12 @@ function DashboardTab() {
                                             <th scope="col" className="px-6 py-3">
                                                 Uid
                                             </th>
-
                                         </tr>
                                     </thead>
                                     {user.map((item, index) => {
                                         const { name, uid, email, date } = item;
                                         return (
-                                            <tbody>
+                                            <tbody key={index}>
                                                 <tr className="bg-gray-50 border-b  dark:border-gray-700" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                                                     <td className="px-6 py-4 text-black " style={{ color: mode === 'dark' ? 'white' : '' }}>
                                                         {index + 1}.
@@ -232,7 +223,6 @@ function DashboardTab() {
                                                     <td className="px-6 py-4 text-black " style={{ color: mode === 'dark' ? 'white' : '' }}>
                                                         {uid}
                                                     </td>
-
                                                 </tr>
                                             </tbody>
                                         )
@@ -248,5 +238,4 @@ function DashboardTab() {
     )
 }
 
-
-export default DashboardTab
+export default DashboardTab;
